9c44986c0dc53127abc2383e0bb949a59475f7c5
Nishant Singh <135511666+NishantSingh004@users.noreply.github.com>
2023-08-10 04:10:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import data
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+url = "https://www.iplt20.com/auction/2022"
+r = requests.get(url)
+print(r)
+
+soup = BeautifulSoup(r.content, "html.parser")
+# print(soup)
+table = soup.find("table", class_ = "ih-td-tab auction-tbl")
+# print(table)
+title = table.find_all("th")
+# print(title)
+
+header  = []
+for i in title:
+    name = i.text
+    header.append(name)
+
+df = pd.DataFrame(columns=header)
+rows = table.find_all("tr")
+for i in rows[1:]:
+    first_td = i.find_all("td")[0].find("div",  class_="ih-pt-ic").text.strip()
+    data =  i.find_all("td")[1:]
+    row = [tr.text for tr in data]
+    row.insert(0, first_td) #to insert data in each row
+    l = len(df)
+    df.loc[l] = row
+    print(df)
+df.to_csv("ipl Auction stats.csv")
